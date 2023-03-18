03b712410c7ad718c96d3e381d34a42a152cd3a5
SirDouglas516 <douglasogaga52@gmail.com>
2023-03-17 18:58:31 -0700

    variables shell

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse,
+ * followed by a new line.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char low;
+for (low = 'z'; low >= 'a'; low--)
+putchar(low);
+putchar('\n');
+
+return (0);
+}
