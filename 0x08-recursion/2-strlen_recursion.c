d7a652894fcb2e5e12a19ed9f172de76a7415734
SirDouglas516 <douglasogaga52@gmail.com>
2023-04-06 13:26:49 -0700

    program

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,10 +9,10 @@ int _strlen_recursion(char *s)
 {
 	int longit = 0;
 
-	if(*s)
+	if (*s)
 	{
 		longit++;
-		longit +=_strlen_recursion(s + 1);
+		longit += _strlen_recursion(s + 1);
 	}
 	return (longit);
 }
