6df4b55944cabc393a4761c6161415a2764e7f30
SirDouglas516 <douglasogaga52@gmail.com>
2023-04-06 17:43:44 -0700

    program

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 
 /**
  * str_checker - check if two strings are identical.
@@ -12,10 +12,10 @@ int str_checker(char *s1, char *s2, int i, int j)
 {
 	if (s1[i] == '\0' && s2[j] == '\0')
 		return (1);
-	if (s1[i] == s@[j])
+	if (s1[i] == s2[j])
 		return (str_checker(s1, s2, i + 1, j + 1));
 	if (s1[1] == '\0' && s2[j] == '*')
-		return (str_checkers1(s1, s2, i, j + 1));
+		return (str_checker(s1, s2, i, j + 1));
 	if (s2[j] == '*')
 		return (str_checker(s1, s2, i + 1, j) || str_checker(s1, s2, i, j + 1));
 	return (0);
