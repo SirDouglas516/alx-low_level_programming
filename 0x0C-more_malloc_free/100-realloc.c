f027ac48e531385327fb2b094b601adf66ff2091
SirDouglas516 <douglasogaga52@gmail.com>
2023-04-13 10:42:49 -0700

    malloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -8,8 +8,7 @@
  * @new_size: new size of the new memory block
  *
  * Return: pointer to the newly allocated memory block
- */ 
-
+ */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 char *ptr1;
